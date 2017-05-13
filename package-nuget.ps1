30624a17d66bfd1cecb12b5591f60d6d1717f392
James Randall <james@accidentalfish.com>
2017-05-13 16:09:24 +0100

    Package and folder rename

---
+++ package-nuget.ps1
@@ -0,0 +1,36 @@
+﻿Param(
+	[switch]$pushLocal,
+	[switch]$pushNuget,
+	[switch]$cleanup
+)
+
+if (Test-Path -Path nuget-powershell) 
+{
+	rmdir nuget-powershell -Recurse
+}
+if (Test-Path -Path nuget-cmdline) 
+{
+	rmdir nuget-cmdline -Recurse
+}
+rm *.nupkg
+
+msbuild
+
+if ($pushLocal)
+{
+	cp .\Source\AccidentalFish.Commanding\bin\debug\*.nupkg \MicroserviceAnalyticPackageRepository
+	cp .\Source\AccidentalFish.Commanding.AzureStorage\bin\debug\*.nupkg \MicroserviceAnalyticPackageRepository
+	cp .\Source\AccidentalFish.Commanding.Http\bin\debug\*.nupkg \MicroserviceAnalyticPackageRepository
+	cp .\Source\AccidentalFish.Commanding.Queue\bin\debug\*.nupkg \MicroserviceAnalyticPackageRepository	
+}
+
+if ($pushNuget)
+{
+	.\nuget push *.nupkg -source nuget.org
+}
+
+if ($cleanup)
+{
+	rmdir nuget-powershell -Recurse
+	rm *.nupkg
+}
