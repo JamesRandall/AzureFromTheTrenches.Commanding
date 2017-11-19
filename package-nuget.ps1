49d65f31f5ccad735beafe1e8e57ee9abe452940
James Randall <james@accidentalfish.com>
2017-11-19 14:21:23 +0000

    Folder renames

--- package-nuget.ps1
+++ package-nuget.ps1
@@ -12,37 +12,37 @@ if (Test-Path -Path nuget-cmdline)
 	rmdir nuget-cmdline -Recurse
 }
 
-rm .\Source\AccidentalFish.Commanding.Abstractions\bin\debug\*.nupkg
-rm .\Source\AccidentalFish.Commanding\bin\debug\*.nupkg
-rm .\Source\AccidentalFish.Commanding.AzureStorage\bin\debug\*.nupkg
-rm .\Source\AccidentalFish.Commanding.Http\bin\debug\*.nupkg
-rm .\Source\AccidentalFish.Commanding.Queue\bin\debug\*.nupkg
-rm .\Source\AccidentalFish.Commanding.Cache\bin\debug\*.nupkg
-rm .\Source\AccidentalFish.Commanding.Cache.MemoryCache\bin\debug\*.nupkg
-rm .\Source\AccidentalFish.Commanding.Cache.Redis\bin\debug\*.nupkg
+rm .\Source\AzureFromTheTrenches.Commanding.Abstractions\bin\debug\*.nupkg
+rm .\Source\AzureFromTheTrenches.Commanding\bin\debug\*.nupkg
+rm .\Source\AzureFromTheTrenches.Commanding.AzureStorage\bin\debug\*.nupkg
+rm .\Source\AzureFromTheTrenches.Commanding.Http\bin\debug\*.nupkg
+rm .\Source\AzureFromTheTrenches.Commanding.Queue\bin\debug\*.nupkg
+rm .\Source\AzureFromTheTrenches.Commanding.Cache\bin\debug\*.nupkg
+rm .\Source\AzureFromTheTrenches.Commanding.Cache.MemoryCache\bin\debug\*.nupkg
+rm .\Source\AzureFromTheTrenches.Commanding.Cache.Redis\bin\debug\*.nupkg
 
 msbuild
 
 if ($pushLocal)
 {
-	cp .\Source\AccidentalFish.Commanding.Abstractions\bin\debug\*.nupkg \MicroserviceAnalyticPackageRepository
-	cp .\Source\AccidentalFish.Commanding\bin\debug\*.nupkg \MicroserviceAnalyticPackageRepository
-	cp .\Source\AccidentalFish.Commanding.AzureStorage\bin\debug\*.nupkg \MicroserviceAnalyticPackageRepository
-	cp .\Source\AccidentalFish.Commanding.Http\bin\debug\*.nupkg \MicroserviceAnalyticPackageRepository
-	cp .\Source\AccidentalFish.Commanding.Queue\bin\debug\*.nupkg \MicroserviceAnalyticPackageRepository	
-	cp .\Source\AccidentalFish.Commanding.Cache\bin\debug\*.nupkg \MicroserviceAnalyticPackageRepository	
-	cp .\Source\AccidentalFish.Commanding.Cache.MemoryCache\bin\debug\*.nupkg \MicroserviceAnalyticPackageRepository	
-	cp .\Source\AccidentalFish.Commanding.Cache.Redis\bin\debug\*.nupkg \MicroserviceAnalyticPackageRepository	
+	cp .\Source\AzureFromTheTrenches.Commanding.Abstractions\bin\debug\*.nupkg \MicroserviceAnalyticPackageRepository
+	cp .\Source\AzureFromTheTrenches.Commanding\bin\debug\*.nupkg \MicroserviceAnalyticPackageRepository
+	cp .\Source\AzureFromTheTrenches.Commanding.AzureStorage\bin\debug\*.nupkg \MicroserviceAnalyticPackageRepository
+	cp .\Source\AzureFromTheTrenches.Commanding.Http\bin\debug\*.nupkg \MicroserviceAnalyticPackageRepository
+	cp .\Source\AzureFromTheTrenches.Commanding.Queue\bin\debug\*.nupkg \MicroserviceAnalyticPackageRepository	
+	cp .\Source\AzureFromTheTrenches.Commanding.Cache\bin\debug\*.nupkg \MicroserviceAnalyticPackageRepository	
+	cp .\Source\AzureFromTheTrenches.Commanding.Cache.MemoryCache\bin\debug\*.nupkg \MicroserviceAnalyticPackageRepository	
+	cp .\Source\AzureFromTheTrenches.Commanding.Cache.Redis\bin\debug\*.nupkg \MicroserviceAnalyticPackageRepository	
 }
 
 if ($pushNuget)
 {
-	dotnet nuget push .\Source\AccidentalFish.Commanding.Abstractions\bin\debug\*.nupkg --source nuget.org
-	dotnet nuget push .\Source\AccidentalFish.Commanding\bin\debug\*.nupkg --source nuget.org
-	dotnet nuget push .\Source\AccidentalFish.Commanding.AzureStorage\bin\debug\*.nupkg --source nuget.org
-	dotnet nuget push .\Source\AccidentalFish.Commanding.Http\bin\debug\*.nupkg --source nuget.org
-	dotnet nuget push .\Source\AccidentalFish.Commanding.Queue\bin\debug\*.nupkg --source nuget.org
-	dotnet nuget push .\Source\AccidentalFish.Commanding.Cache\bin\debug\*.nupkg --source nuget.org
-	dotnet nuget push .\Source\AccidentalFish.Commanding.Cache.MemoryCache\bin\debug\*.nupkg --source nuget.org
-	dotnet nuget push .\Source\AccidentalFish.Commanding.Cache.Redis\bin\debug\*.nupkg --source nuget.org
+	dotnet nuget push .\Source\AzureFromTheTrenches.Commanding.Abstractions\bin\debug\*.nupkg --source nuget.org
+	dotnet nuget push .\Source\AzureFromTheTrenches.Commanding\bin\debug\*.nupkg --source nuget.org
+	dotnet nuget push .\Source\AzureFromTheTrenches.Commanding.AzureStorage\bin\debug\*.nupkg --source nuget.org
+	dotnet nuget push .\Source\AzureFromTheTrenches.Commanding.Http\bin\debug\*.nupkg --source nuget.org
+	dotnet nuget push .\Source\AzureFromTheTrenches.Commanding.Queue\bin\debug\*.nupkg --source nuget.org
+	dotnet nuget push .\Source\AzureFromTheTrenches.Commanding.Cache\bin\debug\*.nupkg --source nuget.org
+	dotnet nuget push .\Source\AzureFromTheTrenches.Commanding.Cache.MemoryCache\bin\debug\*.nupkg --source nuget.org
+	dotnet nuget push .\Source\AzureFromTheTrenches.Commanding.Cache.Redis\bin\debug\*.nupkg --source nuget.org
 }
