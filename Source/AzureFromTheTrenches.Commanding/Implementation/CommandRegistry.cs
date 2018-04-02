07ef255d351a2e93fa6342c00881fb00013a6f83
James Randall <james@accidentalfish.com>
2018-04-02 16:31:29 +0100

    Updates to registry

--- Source/AzureFromTheTrenches.Commanding/Implementation/CommandRegistry.cs
+++ Source/AzureFromTheTrenches.Commanding/Implementation/CommandRegistry.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using AzureFromTheTrenches.Commanding.Abstractions;
 using AzureFromTheTrenches.Commanding.Abstractions.Model;
 using AzureFromTheTrenches.Commanding.Model;
