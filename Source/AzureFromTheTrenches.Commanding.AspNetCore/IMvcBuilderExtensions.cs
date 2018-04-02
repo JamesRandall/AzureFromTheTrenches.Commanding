9c139718bb17859b30674cfa2cbb27160a756c84
James Randall <james@accidentalfish.com>
2018-04-02 10:08:27 +0100

    Comments, cleanup, versioning

--- Source/AzureFromTheTrenches.Commanding.AspNetCore/IMvcBuilderExtensions.cs
+++ Source/AzureFromTheTrenches.Commanding.AspNetCore/IMvcBuilderExtensions.cs
@@ -24,6 +24,8 @@ namespace AzureFromTheTrenches.Commanding.AspNetCore
             configurationBuilder(restCommandBuilderInstance);
             restCommandBuilderInstance.SetDefaultNamespaceOnControllers();
 
+            // When the user has completed defining their command based API through the builder we compile controllers, using Roslyn, using the definitions
+            // that have been supplied
             ISyntaxTreeCompiler syntaxTreeCompiler = new SyntaxTreeCompiler();
             IControllerTemplateCompiler controllerTemplateCompiler = new HandlebarsControllerTemplateCompiler(restCommandBuilderInstance.ExternalTemplateProvider);
             IControllerCompiler controllerCompiler = new ControllerCompiler(
@@ -34,8 +36,12 @@ namespace AzureFromTheTrenches.Commanding.AspNetCore
                 restCommandBuilderInstance.OutputNamespace);
             mvcBuilder.AddApplicationPart(assembly);
 
+            // We pre-compile claim to command mappers for each kind of command in use - these are used by the model binders below
             ICommandClaimsBinderProvider commandClaimsBinderProvider = restCommandBuilderInstance.ClaimsMappingBuilder.Build(restCommandBuilderInstance.GetRegisteredCommandTypes());
 
+            // Here we add model binders that are able to map claims onto commands. These deliberately can write to the SecurityProperty marked attribute
+            // of commands as these are very likely to come from claims. Binders are registered for body, path (URI), and query strings.
+            // These wrap the appropriate underlying binder so that the actual model binding takes place using the existing built in binders.
             mvcBuilder.AddMvcOptions(options =>
             {
                 IModelBinderProvider bodyModelBinderProvider = options.ModelBinderProviders.Single(x => x is BodyModelBinderProvider);
--- Source/AzureFromTheTrenches.Commanding.AspNetCore/Implementation/ControllerCompiler.cs
+++ Source/AzureFromTheTrenches.Commanding.AspNetCore/Implementation/ControllerCompiler.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
-using AzureFromTheTrenches.Commanding.AspNetCore.Implementation.Model;
 using AzureFromTheTrenches.Commanding.AspNetCore.Model;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
@@ -13,15 +12,12 @@ namespace AzureFromTheTrenches.Commanding.AspNetCore.Implementation
     {
         private readonly IControllerTemplateCompiler _controllerTemplateCompiler;
         private readonly ISyntaxTreeCompiler _syntaxTreeCompiler;
-        private readonly ClaimsMappingBuilder _claimsMappingBuilder;
 
         public ControllerCompiler(IControllerTemplateCompiler controllerTemplateCompiler,
-            ISyntaxTreeCompiler syntaxTreeCompiler,
-            ClaimsMappingBuilder claimsMappingBuilder)
+            ISyntaxTreeCompiler syntaxTreeCompiler)
         {
             _controllerTemplateCompiler = controllerTemplateCompiler;
             _syntaxTreeCompiler = syntaxTreeCompiler;
-            _claimsMappingBuilder = claimsMappingBuilder;
         }
 
         public Assembly Compile(IReadOnlyCollection<ControllerDefinition> definitions, string outputNamespaceName)
@@ -33,7 +29,7 @@ namespace AzureFromTheTrenches.Commanding.AspNetCore.Implementation
             {
                 Func<object,string> template = templates[definition.Name];
                 
-                string controllerClassSource = template(AugmentedControllerDefinition.FromControllerDefinition(definition, _claimsMappingBuilder));
+                string controllerClassSource = template(definition);
 
                 SyntaxTree syntaxTree = CSharpSyntaxTree.ParseText(controllerClassSource);
                 syntaxTrees.Add(syntaxTree);
