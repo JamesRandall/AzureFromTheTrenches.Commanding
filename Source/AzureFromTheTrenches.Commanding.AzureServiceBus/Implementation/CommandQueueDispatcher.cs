40b32d88f78e86b7bd0c09fb07a47ddb51ab160d
James Randall <james@accidentalfish.com>
2019-06-11 10:39:29 +0100

    Service bus update

--- Source/AzureFromTheTrenches.Commanding.AzureServiceBus/Implementation/CommandQueueDispatcher.cs
+++ Source/AzureFromTheTrenches.Commanding.AzureServiceBus/Implementation/CommandQueueDispatcher.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using AzureFromTheTrenches.Commanding.Abstractions;
@@ -31,10 +32,37 @@ namespace AzureFromTheTrenches.Commanding.AzureServiceBus.Implementation
         {
             byte[] messageBytes= _serializer.Serialize(command);
             Message message = new Message(messageBytes);
+            message.SessionId = GetSessionId(command);
             await _queueClient.SendAsync(message);
             return new CommandResult(true);
         }
 
         public ICommandExecuter AssociatedExecuter { get; }
+
+        protected virtual string GetSessionId(ICommand command)
+        {
+            return null;
+        }
+    }
+
+    class CommandQueueDispatcher<TCommand> : CommandQueueDispatcher where TCommand : ICommand
+    {
+        private readonly Func<TCommand, string> _sessionIdProvider;
+
+        public CommandQueueDispatcher(QueueClient queueClient, IServiceBusMessageSerializer serializer, Func<TCommand, string> sessionIdProvider) : base(queueClient, serializer)
+        {
+            _sessionIdProvider = sessionIdProvider;
+        }
+
+        protected override string GetSessionId(ICommand command)
+        {
+            if (_sessionIdProvider == null)
+            {
+                return null;
+            }
+
+            string sessionId = _sessionIdProvider((TCommand) command);
+            return sessionId;
+        }
     }
 }
--- Source/AzureFromTheTrenches.Commanding.AzureServiceBus/QueueClientExtensions.cs
+++ Source/AzureFromTheTrenches.Commanding.AzureServiceBus/QueueClientExtensions.cs
@@ -13,12 +13,28 @@ namespace AzureFromTheTrenches.Commanding.AzureServiceBus
         {
             return new CommandQueueDispatcher(queueClient, serializer ?? new JsonServiceBusMessageSerializer());
         }
+        
+        public static ICommandDispatcher CreateCommandDispatcher<TCommand>(
+            this QueueClient queueClient,
+            IServiceBusMessageSerializer serializer = null,
+            Func<TCommand, string> sessionIdProvider = null) where TCommand : ICommand
+        {
+            return new CommandQueueDispatcher<TCommand>(queueClient, serializer ?? new JsonServiceBusMessageSerializer(), sessionIdProvider);
+        }
 
         public static Func<ICommandDispatcher> CreateCommandDispatcherFactory(
             this QueueClient queueClient,
             IServiceBusMessageSerializer serializer = null)
         {
             return () => new CommandQueueDispatcher(queueClient, serializer ?? new JsonServiceBusMessageSerializer());
+        }
+        
+        public static Func<ICommandDispatcher> CreateCommandDispatcherFactory<TCommand>(
+            this QueueClient queueClient,
+            IServiceBusMessageSerializer serializer = null,
+            Func<TCommand, string> sessionIdProvider = null) where TCommand : ICommand
+        {
+            return () => new CommandQueueDispatcher<TCommand>(queueClient, serializer ?? new JsonServiceBusMessageSerializer(), sessionIdProvider);
         }
     }
 }
